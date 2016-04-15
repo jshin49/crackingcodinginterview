b09a374db2e6ef4a2d4bde66b5bbd937b1c4e469
Michael Baldwin <baldwmic@mail.gvsu.edu>
2016-04-14 22:19:39 -0400

    python practice starting

---
+++ other/codingHorror/hackerfellows/ReverseString.java
@@ -0,0 +1,11 @@
+public class ReverseString {
+
+    public static reverseString(String original) {
+        char[] chars = original.toCharArray();
+        for (int i = 0; i < chars.length /
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
